f1313acdcb7d82f4376f9d8f7b55a0a0a6a9e757
”Roman <crvnsk@gmail.com>
2023-04-22 12:49:20 +0700

    changes name constant

--- temp.py
+++ temp.py
@@ -19,14 +19,15 @@ def my_func():
     Returns:
         _type_: _description_
     """
-    A=1
-    VAR_B = 2
-    VAR_C = VAR_A + VAR_B
-    print(VAR_C)
-    return VAR_C
+    VAR_a = 1
+    VAR_b = 2
+    VAR_c = VAR_a + VAR_b
+    print(VAR_c)
+    return VAR_c
 
 
 S = "iamstring"
 A = S[3:5:1]
 
 print(my_func())
+
